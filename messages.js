1709e53fdfff40a2cc5d5f33235c71cb99e14416
Tobbe Lundberg <tobbe@tlundberg.com>
2026-03-10 11:30:14 +0100

    update tunnel url

--- messages.js
+++ messages.js
@@ -1,10 +1,12 @@
+const url = "https://least-despite-outlets-crossword.trycloudflare.com";
+
 export async function handleMessage(msg) {
   console.log("Handling message:", msg);
 
   if (msg.toLowerCase().includes("led on")) {
     // curl -X POST -d LED_ON https://span-pat-blues-carmen.trycloudflare.com
     console.log("Firing off a fetch with LED_ON");
-    const res = await fetch("https://span-pat-blues-carmen.trycloudflare.com", {
+    const res = await fetch(url, {
       method: "POST",
       headers: { "Content-Type": "text/plain" },
       body: "LED_ON",
@@ -13,7 +15,7 @@ export async function handleMessage(msg) {
   } else if (msg.toLowerCase().includes("led off")) {
     // curl -X POST -d LED_OFF https://span-pat-blues-carmen.trycloudflare.com
     console.log("Firing off a fetch with LED_OFF");
-    const res = await fetch("https://span-pat-blues-carmen.trycloudflare.com", {
+    const res = await fetch(url, {
       method: "POST",
       headers: { "Content-Type": "text/plain" },
       body: "LED_OFF",
